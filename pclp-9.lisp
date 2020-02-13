2836fcf271be88f9889e3f7dfdee205c5735cf35
zhujiang <44894157+chrzjsin@users.noreply.github.com>
2020-02-13 15:27:11 +0800

    第九章实践
    第九章编写的单元测试框架

---
+++ pclp-9.lisp
@@ -0,0 +1,25 @@
+;;;;µÚ¾ÅÕÂµÄ²âÊÔ¿ò¼Ü
+
+(defvar *test-name* nil)
+
+(defmacro deftest (name parameters &body body)
+  `(defun ,name ,parameters
+     (let ((*test-name* (append *test-name* (list ',name))))
+       ,@body)))
+
+(defun report-result (result form)
+  (format t "~:[FAIL~;pass~]...~a: ~a~%" result *test-name* form)
+  result)
+
+(defmacro with-gensyms ( (&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+(defmacro combine-results (&body forms)
+  (with-gensyms (result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
+(defmacro check (&body forms)
+  `(combine-results
+     ,@(loop for f in forms collect `(report-result ,f ',f))))
